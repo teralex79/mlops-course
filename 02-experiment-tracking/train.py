bcddf0652e3485ddeba0617bd943c8799aa068a3
Aleksei Terentev <44712948+teralex79@users.noreply.github.com>
2024-11-07 18:05:27 +0000

    modified with autolog

--- 02-experiment-tracking/train.py
+++ 02-experiment-tracking/train.py
@@ -1,10 +1,16 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("nyc-taxi-exp")
+
+mlflow.autolog()
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -22,11 +28,12 @@ def run_train(data_path: str):
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    with mlflow.start_run():
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
 
 
 if __name__ == '__main__':
