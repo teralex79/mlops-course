7f1f9f84d0656acae7161dcffea10e621ba38e77
Aleksei Terentev <44712948+teralex79@users.noreply.github.com>
2024-11-11 19:28:54 +0000

    tracked the experiment

--- 02-experiment-tracking/hpo.py
+++ 02-experiment-tracking/hpo.py
@@ -35,10 +35,16 @@ def run_optimization(data_path: str, num_trials: int):
 
     def objective(params):
 
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_val)
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        with mlflow.start_run():
+
+            mlflow.log_params(params)
+
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            rmse = mean_squared_error(y_val, y_pred, squared=False)
+            
+            mlflow.log_metric("rmse", rmse)
 
         return {'loss': rmse, 'status': STATUS_OK}
 
