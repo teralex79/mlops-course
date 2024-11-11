6e2b260c1564d36552c8bb6029a40a0c620d1a42
Aleksei Terentev <44712948+teralex79@users.noreply.github.com>
2024-11-11 22:30:12 +0000

    modified with register best_run

--- 02-experiment-tracking/register_model.py
+++ 02-experiment-tracking/register_model.py
@@ -71,10 +71,22 @@ def run_register_model(data_path: str, top_n: int):
 
     # Select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs( 
+        
+        experiment_ids=experiment.experiment_id,
+        filter_string="",
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=top_n,
+        order_by=["metrics.test_rmse ASC"]
+    )[0]
 
     # Register the best model
-    # mlflow.register_model( ... )
+    run_id = best_run.info.run_id
+    mlflow.register_model( 
+        
+        model_uri=f"runs:/{run_id}/models",
+        name='best_run'
+    )
 
 
 if __name__ == '__main__':
--- 02-experiment-tracking/train.py
+++ 02-experiment-tracking/train.py
@@ -34,7 +34,9 @@ def run_train(data_path: str):
         y_pred = rf.predict(X_val)
 
         rmse = mean_squared_error(y_val, y_pred, squared=False)
+        # mlflow.log_metric("rmse", rmse)
 
+        # mlflow.log_artifact()
 
 if __name__ == '__main__':
     run_train()
