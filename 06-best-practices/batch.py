5f29ab5ce9fc590b766c61528ebc0e143868f071
Aleksei Terentev <44712948+teralex79@users.noreply.github.com>
2024-12-11 20:33:58 +0000

    completed hw06.q4

--- 06-best-practices/batch.py
+++ 06-best-practices/batch.py
@@ -1,10 +1,13 @@
 # pylint: disable=missing-module-docstring
 # pylint: disable=line-too-long
+# pylint: disable=C0103
+
 #!/usr/bin/env python
 # coding: utf-8
 
 import sys
 import pickle
+import os
 import pandas as pd
 
 
@@ -24,9 +27,20 @@ def prepare_data(df, categorical):
 def read_data(filename, categorical):
     """Read data function"""
 
-    df = pd.read_parquet(filename)
-    df = prepare_data(df, categorical)
+    S3_ENDPOINT_URL = os.getenv('S3_ENDPOINT_URL')
 
+    if S3_ENDPOINT_URL is not None:
+        options = {
+            'client_kwargs': {
+                'endpoint_url': S3_ENDPOINT_URL
+            }
+        }
+
+        df = pd.read_parquet(filename, storage_options=options)
+    else:
+        df = pd.read_parquet(filename)
+ 
+    df = prepare_data(df, categorical)
     return df
 
 
@@ -47,14 +61,41 @@ def load_model(input_file: str, dv, lr, output_file: str, year, month):
     df_result['ride_id'] = df['ride_id']
     df_result['predicted_duration'] = y_pred
 
-    df_result.to_parquet(output_file, engine='pyarrow', index=False)
+    S3_ENDPOINT_URL = os.getenv('S3_ENDPOINT_URL')
+
+    if S3_ENDPOINT_URL is not None:
+        options = {
+            'client_kwargs': {
+                'endpoint_url': S3_ENDPOINT_URL
+            }
+        }
+
+        df_result.to_parquet(output_file, engine='pyarrow', index=False, storage_options=options)
+    else:
+        df_result.to_parquet(output_file, engine='pyarrow', index=False)
+
+
+def get_input_path(taxi_type, year, month):
+    default_input_pattern = 'https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_type}_tripdata_{year:04d}-{month:02d}.parquet'
+    input_pattern = os.getenv('INPUT_FILE_PATTERN', default_input_pattern)
+    return input_pattern.format(taxi_type=taxi_type, year=year, month=month)
+
+
+def get_output_path(year, month):
+    default_output_pattern = 's3://nyc-duration-prediction-alexey/taxi_type=fhv/year={year:04d}/month={month:02d}/predictions.parquet'
+    output_pattern = os.getenv('OUTPUT_FILE_PATTERN', default_output_pattern)
+    return output_pattern.format(year=year, month=month)
 
 
 def main(taxi_type, year, month):
     """Main function"""
 
-    input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_type}_tripdata_{year:04d}-{month:02d}.parquet'
-    output_file = f'output/{taxi_type}_tripdata_{year:04d}-{month:02d}.parquet'
+ #   input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_type}_tripdata_{year:04d}-{month:02d}.parquet'
+ #   output_file = f'output/{taxi_type}_tripdata_{year:04d}-{month:02d}.parquet'
+
+    input_file = get_input_path(taxi_type, year, month)
+    output_file = get_output_path(year, month)
+
     with open('model.bin', 'rb') as f_in:
         dv, lr = pickle.load(f_in)
 
@@ -62,8 +103,8 @@ def main(taxi_type, year, month):
         input_file=input_file,
         dv=dv,
         lr=lr,
-        output_file=output_file, 
-        year=year, 
+        output_file=output_file,
+        year=year,
         month=month
     )
 
