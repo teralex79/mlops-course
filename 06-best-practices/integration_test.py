8ffb32ffbbf1f616aef603b0136bb71d31150ae9
Aleksei Terentev <44712948+teralex79@users.noreply.github.com>
2024-12-11 22:03:04 +0000

    completed hw06.q5

--- 06-best-practices/integration_test.py
+++ 06-best-practices/integration_test.py
@@ -11,17 +11,7 @@ from batch import get_input_path
 
 
 def dt(hour, minute, second=0):
-    return datetime(2023, 1, 1, hour, minute, second)
-
-data = [
-    (None, None, dt(1, 1), dt(1, 10)),
-    (1, 1, dt(1, 2), dt(1, 10)),
-    (1, None, dt(1, 2, 0), dt(1, 2, 59)),
-    (3, 4, dt(1, 2, 0), dt(2, 2, 1)),      
-]
-
-columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']
-df_input = pd.DataFrame(data, columns=columns)
+    return datetime(2024, 1, 1, hour, minute, second)
 
 S3_ENDPOINT_URL = os.getenv('S3_ENDPOINT_URL')
 
@@ -31,7 +21,17 @@ options = {
     }
 }
 
-input_file = get_input_path('yellow', 2022, 1)
+data = [
+    (None, None, dt(1, 1), dt(1, 10)),
+    (1, 1, dt(1, 2), dt(1, 10)),
+    (1, None, dt(1, 2, 0), dt(1, 2, 59)),
+    (3, 4, dt(1, 2, 0), dt(2, 2, 1)),
+]
+
+columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']
+df_input = pd.DataFrame(data, columns=columns)
+
+input_file = get_input_path('yellow', 2023, 1)
 
 df_input.to_parquet(
     input_file,
