c48bbf43b4bfe4810d1514b305bcba9828962d2b
Aleksei Terentev <44712948+teralex79@users.noreply.github.com>
2024-12-11 21:51:30 +0000

    completed hw06.q5

---
+++ 06-best-practices/integration_test.py
@@ -0,0 +1,42 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=line-too-long
+
+from datetime import datetime
+
+import os
+import pandas as pd
+
+from batch import get_input_path
+
+
+def dt(hour, minute, second=0):
+    return datetime(2023, 1, 1, hour, minute, second)
+
+data = [
+    (None, None, dt(1, 1), dt(1, 10)),
+    (1, 1, dt(1, 2), dt(1, 10)),
+    (1, None, dt(1, 2, 0), dt(1, 2, 59)),
+    (3, 4, dt(1, 2, 0), dt(2, 2, 1)),      
+]
+
+columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']
+df_input = pd.DataFrame(data, columns=columns)
+
+S3_ENDPOINT_URL = os.getenv('S3_ENDPOINT_URL')
+
+options = {
+    'client_kwargs': {
+        'endpoint_url': S3_ENDPOINT_URL
+    }
+}
+
+input_file = get_input_path('yellow', 2022, 1)
+
+df_input.to_parquet(
+    input_file,
+    engine='pyarrow',
+    compression=None,
+    index=False,
+    storage_options=options
+)
